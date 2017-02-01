0a4813b5037717ab141e3f3ed6f56480cf706719
Gabriel Ranghetti <gabriel.ranghetti@agenciamilagro.com.br>
2017-02-01 11:57:30 -0200

    first commit

---
+++ app/containers/AppContainer.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import {
+    View,
+    Text,
+    TouchableHighlight
+} from 'react-native';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { ActionCreators } from '../actions';
+
+class AppContainer extends Component
+{
+    addRecipe()
+    {
+        this.props.addRecipe();
+    }
+
+    render()
+    {
+        return (
+            <View>
+                <Text style={{marginTop: 20}}>
+                    Im a App Container! Recipe Count: {this.props.recipeCount}
+                </Text>
+                <TouchableHighlight onPress={() => {this.addRecipe()}}>
+                    <Text>Add recipe</Text>
+                </TouchableHighlight>
+            </View>
+        );
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators(ActionCreators, dispatch);
+}
+
+export default connect((state) => {
+    return {
+        recipeCount: state.recipeCount
+    }
+}, mapDispatchToProps)(AppContainer);
---
+++ index.ios.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import {
+    AppRegistry
+} from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import createLogger from 'redux-logger';
+import reducer from './app/reducers';
+import AppContainer from './app/containers/AppContainer';
+
+const loggerMiddleware = createLogger({ predicate: (getState, action) => __DEV__ });
+const store = configureStore({});
+
+function configureStore(initialState) {
+    const enhancer = compose(
+        applyMiddleware(
+            thunkMiddleware,
+            loggerMiddleware
+        )
+    );
+    return createStore(reducer, initialState, enhancer);
+}
+
+const App = () => (
+    <Provider store={store}>
+        <AppContainer/>
+    </Provider>
+);
+
+//TODO Insert project name
+AppRegistry.registerComponent('', () => App);
